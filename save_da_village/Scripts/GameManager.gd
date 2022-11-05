6f4eb1b4568c27cedcb009d8ca2c952bf5275d48
Luke Sequeira <lukemsequeira@gmail.com>
2022-11-05 09:13:10 -0400

    Changed block sizes to match tilemap size (32) I also moved the gameManager to the front layer

--- save_da_village/Scripts/GameManager.gd
+++ save_da_village/Scripts/GameManager.gd
@@ -7,7 +7,7 @@ const X_SIZE = 960
 const Y_SIZE = 560
 
 const GRID_INTERACT_SCRIPT = preload("res://Scripts/Grid/GridInteract.gd")
-
+const GRID_BLOCK_SIZE = 32
 
 var sensorGrid = []
 
@@ -15,11 +15,11 @@ var sensorGrid = []
 func _ready():
 	
 	# Populate sensorGrid with GridInteracts
-	for i in range(X_SIZE/40):
+	for i in range(X_SIZE/GRID_BLOCK_SIZE):
 		sensorGrid.append([])
-		for j in range(Y_SIZE/40):
+		for j in range(Y_SIZE/GRID_BLOCK_SIZE):
 			var newNode = ColorRect.new()
-			newNode.set_position(Vector2(i*40, 560 - j*40))
+			newNode.set_position(Vector2(i*GRID_BLOCK_SIZE, 560 - j*GRID_BLOCK_SIZE))
 			newNode.set_script(GRID_INTERACT_SCRIPT)
 			newNode.posx = i
 			newNode.posy = j
