c741ad70761a937c77fe56cbf4f66122ca117c9e
Yohello1 <mp2702737@gmail.com>
2022-11-05 18:08:37 -0400

    GRavity

--- save_da_village/assets/villager_0.gd
+++ save_da_village/assets/villager_0.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
-var movement = RandomNumberGenerator.new()
+var rand = RandomNumberGenerator.new()
 export(int) var speed = 40.0
+var motion = Vector2()
+var UP = Vector2(0,-1)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +14,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var motion = Vector2()
-	movement.randomize()
-	var move = movement.randf_range(-100, 100)
-	motion.x += move
-
-	move_and_slide(motion * speed)
+	rand.randomize()
+	if is_on_floor() == true:
+		motion.y = 400
+	if is_on_floor() == false:
+		if motion.y <= 390:
+			motion.y += 98
+	
+	
+	
+	var move = rand.randf_range(-100, 100)
+	
+	motion.x = move
+	
+	move_and_slide(motion, UP)
 	
