29f9fb7c65713050eaa43cbd4a7c9454e235ffcc
laanspoon <tarolanson@gmail.com>
2022-11-05 18:04:12 -0400

    Villager in right spot

--- save_da_village/assets/villager_0.gd
+++ save_da_village/assets/villager_0.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var movement = RandomNumberGenerator.new()
-export(int) var speed = 40.0
+export(int) var speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +12,7 @@ func _ready():
 func _physics_process(delta):
 	var motion = Vector2()
 	movement.randomize()
-	var move = movement.randf_range(-100, 100)
+	var move = movement.randf_range(-25, 25)
 	motion.x += move
 
 	move_and_slide(motion * speed)
-	
