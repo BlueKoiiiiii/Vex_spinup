7d4c04d1d154ee3ca5028efdd8051265372af8fd
Luke Sequeira <lukemsequeira@gmail.com>
2022-11-04 22:17:57 -0400

    Added colour-changing UI tiles Added a script (Grid/GridInteract.gd) that makes transparent blocks grey (partially transparent) upon hover

---
+++ save_da_village/Scripts/Grid/GridInteract.gd
@@ -0,0 +1,28 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+var posx : int
+var posy : int
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	color = Color(color.r, color.g, color.b, 0) # transparent
+	print(color)
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+
+func _on_mouse_entered():
+	color = Color(color.r, color.g, color.b, 93.0/255.0) 	# not transparent
+													# In the future, stop hardcoding this! Make a variable.
+	print("here")
+
+func _on_mouse_exited():
+	color = Color(color.r, color.g, color.b, 0) 	# transparent
+	
+	print("not here")
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
