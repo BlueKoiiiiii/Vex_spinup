ddde4b2115193e70cbedec4ec3c91c6401f99d74
laanspoon <tarolanson@gmail.com>
2022-11-05 17:23:48 -0400

    im trying bro pls move :'O

--- save_da_village/assets/villager_0.gd
+++ save_da_village/assets/villager_0.gd
@@ -1,19 +1,15 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var motion = Vector2()
+var movement = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	#Attributes
-	var speed = [0,0]
 	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	movement.randomize()
+	var move = movement.randf_range(-100, 100)
+	motion.x = move
