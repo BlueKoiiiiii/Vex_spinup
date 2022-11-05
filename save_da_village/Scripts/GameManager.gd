bdf1d314219b533bad19665d24a2a409a2576422
Luke Sequeira <lukemsequeira@gmail.com>
2022-11-04 23:21:57 -0400

    Populate the sensor grid when the game starts

---
+++ save_da_village/Scripts/GameManager.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+
+const X_SIZE = 960
+const Y_SIZE = 560
+
+const GRID_INTERACT_SCRIPT = preload("res://Scripts/Grid/GridInteract.gd")
+
+
+var sensorGrid = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	# Populate sensorGrid with GridInteracts
+	for i in range(X_SIZE/40):
+		sensorGrid.append([])
+		for j in range(Y_SIZE/40):
+			var newNode = ColorRect.new()
+			newNode.set_position(Vector2(i*40, 560 - j*40))
+			newNode.set_script(GRID_INTERACT_SCRIPT)
+			newNode.posx = i
+			newNode.posy = j
+			newNode.set_name("cool")
+			add_child(newNode)
+			sensorGrid[i].append(newNode)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- save_da_village/Scripts/Grid/GridInteract.gd
+++ save_da_village/Scripts/Grid/GridInteract.gd
@@ -8,18 +8,18 @@ var posy : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	color = Color(color.r, color.g, color.b, 0) # transparent
-	print(color)
+	color = Color(color.r, color.g, color.b, 0) 			# transparent
+	rect_size = Vector2(40, 40)
 	connect("mouse_entered", self, "_on_mouse_entered")
 	connect("mouse_exited", self, "_on_mouse_exited")
 
 func _on_mouse_entered():
 	color = Color(color.r, color.g, color.b, 93.0/255.0) 	# not transparent
-													# In the future, stop hardcoding this! Make a variable.
+															# In the future, stop hardcoding this! Make a variable.
 	print("here")
 
 func _on_mouse_exited():
-	color = Color(color.r, color.g, color.b, 0) 	# transparent
+	color = Color(color.r, color.g, color.b, 0) 			# transparent
 	
 	print("not here")
 	
