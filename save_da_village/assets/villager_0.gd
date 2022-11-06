06de22afb2c26ba3c41d80bff7ce1b86107824cf
Yohello1 <mp2702737@gmail.com>
2022-11-05 23:10:15 -0400

    merging

--- save_da_village/assets/villager_0.gd
+++ save_da_village/assets/villager_0.gd
@@ -1,21 +1,50 @@
 extends KinematicBody2D
 
+onready var animation = get_node("frames")
+
 var rand = RandomNumberGenerator.new()
 export(int) var speed = 40.0
 var motion = Vector2()
 var UP = Vector2(0,-1)
-
+var move
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	var timer := Timer.new()
+	self.add_child(timer)
+	timer.wait_time = 8
+	timer.one_shot = true
+	timer.start()
+	timer.connect("timeout",self,"_on_timer_timeout")
+	rand.randomize()
+	move = rand.randf_range(-100, 100)
 	
+	if(move < 0):
+		animation.flip_h = false
+	else:
+		animation.flip_h = true
 	
+	var char_selector = rand.randf_range(1,4)
+	print(char_selector)
+	animation.animation = str(int(char_selector))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var motion = Vector2()
-	movement.randomize()
-	var move = movement.randf_range(-25, 25)
-	motion.x += move
 
-	move_and_slide(motion * speed)
+	if is_on_floor() == true:
+		motion.y = 400
+	if is_on_floor() == false:
+		if motion.y <= 390:
+			motion.y += 98
+			
+	motion.x = move
+#	print(motion.x)
+	move_and_slide(motion, UP)
+	
+
+func _on_timer_timeout():
+	move = rand.randf_range(-100, 100)
+	if(move < 0):
+		animation.flip_h = false
+	else:
+		animation.flip_h = true
